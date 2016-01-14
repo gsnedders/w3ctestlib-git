c6e4f70e546f9a79c3dfa6c7c1512c8336c962d8
Geoffrey Sneddon <geoffers@gmail.com>
2016-01-14 23:44:40 +0100

    Let's turn this into a real package :o!

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# CSS Test Suite Manipulation Library
+# Initial code by fantasai, joint copyright 2010 W3C and Microsoft
+# Licensed under BSD 3-Clause: <http://www.w3.org/Consortium/Legal/2008/03-bsd-license>
+
+from setuptools import setup, find_packages
+
+setup(name='w3ctestlib',
+      version='0.1',
+      description='CSS WG testing tools/library',
+      packages=find_packages(),
+      package_data={'w3ctestlib': ['catalog/*', 'templates/*']},
+     )
---
+++ w3ctestlib/Sources.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# CSS Test Source Manipulation Library
+# Initial code by fantasai, joint copyright 2010 W3C and Microsoft
+# Licensed under BSD 3-Clause: <http://www.w3.org/Consortium/Legal/2008/03-bsd-license>
+
+from ConfigSource import ConfigSource
+from FileSource import FileSource
+from HTMLSource import HTMLSource
+from LineString import LineString
+from Metadata import Metadata
+from NamedDict import NamedDict
+from ReferenceData import ReferenceData
+from ReftestManifest import ReftestManifest
+from SVGSource import SVGSource
+from SourceCache import SourceCache
+from SourceSet import SourceSet
+from SourceTree import SourceTree
+from StringReader import StringReader
+from XHTMLSource import XHTMLSource
+from XMLSource import XMLSource
+
+__all__ = ['ConfigSource', 'FileSource', 'HTMLSource', 'LineString', 'Metadata', 'NamedDict', 'ReferenceData', 'ReftestManifest', 'SVGSource', 'SourceCache', 'SourceSet', 'SourceTree', 'StringReader', 'XHTMLSource', 'XMLSource']
