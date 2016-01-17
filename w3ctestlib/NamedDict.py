a66ff5dfc4937c8be876bebe45a874fec8ff04ad
Geoffrey Sneddon <geoffers@gmail.com>
2016-01-17 12:03:11 +0100

    Rewrite NamedDict to use collections.MutableMapping

--- w3ctestlib/NamedDict.py
+++ w3ctestlib/NamedDict.py
@@ -3,24 +3,24 @@
 # Initial code by fantasai, joint copyright 2010 W3C and Microsoft
 # Licensed under BSD 3-Clause: <http://www.w3.org/Consortium/Legal/2008/03-bsd-license>
 
+from collections import MutableMapping
 
-class NamedDict(object):
-    def get(self, key):
-        if (key in self):
-            return self[key]
-        return None
 
-    def __eq__(self, other):
-        for key in self.__slots__:
-            if (self[key] != other[key]):
-                return False
-        return True
+class NamedDict(MutableMapping):
+    def __getitem__(self, key):
+        if key in self:
+            return getattr(self, key)
+        else:
+            raise KeyError, key
 
-    def __ne__(self, other):
-        for key in self.__slots__:
-            if (self[key] != other[key]):
-                return True
-        return False
+    def __setitem__(self, key, value):
+        if key in self:
+            setattr(self, key, value)
+        else:
+            raise KeyError, key
+
+    def __delitem__(self, key):
+        raise TypeError, "cannot remove from a NamedDict"
 
     def __len__(self):
         return len(self.__slots__)
@@ -30,30 +30,3 @@ class NamedDict(object):
 
     def __contains__(self, key):
         return (key in self.__slots__)
-
-    def copy(self):
-        clone = self.__class__()
-        for key in self.__slots__:
-            clone[key] = self[key]
-        return clone
-
-    def keys(self):
-        return self.__slots__
-
-    def has_key(self, key):
-        return (key in self)
-
-    def items(self):
-        return [(key, self[key]) for key in self.__slots__]
-
-    def iteritems(self):
-        return iter(self.items())
-
-    def iterkeys(self):
-        return self.__iter__()
-
-    def itervalues(self):
-        return iter(self.items())
-
-    def __str__(self):
-        return '{ ' + ', '.join([key + ': ' + str(self[key]) for key in self.__slots__]) + ' }'
--- w3ctestlib/ReferenceData.py
+++ w3ctestlib/ReferenceData.py
@@ -14,26 +14,3 @@ class ReferenceData(NamedDict):
         self.type = type
         self.relpath = relpath
         self.repopath = repopath
-
-    def __getitem__(self, key):
-        if ('name' == key):
-            return self.name
-        if ('type' == key):
-            return self.type
-        if ('relpath' == key):
-            return self.relpath
-        if ('repopath' == key):
-            return self.repopath
-        return None
-
-    def __setitem__(self, key, value):
-        if ('name' == key):
-            self.name = value
-        elif ('type' == key):
-            self.type = value
-        elif ('relpath' == key):
-            self.relpath = value
-        elif ('repopath' == key):
-            self.repopath = value
-        else:
-            raise KeyError()
